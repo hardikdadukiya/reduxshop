7d4653f2c0affc2d121e6aabbb84199864c1d18b
Hardik Dadukiya <Hardik@gmail.com>
2022-06-14 14:46:40 +0530

    small-update

--- src/redux/action/productActions.js
+++ src/redux/action/productActions.js
@@ -19,19 +19,19 @@ export const fetchProduct =  (id) =>{
     }
 };
 
-export const setProducts =( products)=>{
-    return {
-        type: ActionTypes.SET_PRODUCTS,
-        payload: products,
-    };
-};
+// export const setProducts =( products)=>{
+//     return {
+//         type: ActionTypes.SET_PRODUCTS,
+//         payload: products,
+//     };
+// };
 
-export const selectedProducts =( product)=>{
-    return {
-        type: ActionTypes.SELECTED_PRODUCTS,
-        payload: product,
-    };
-};
+// export const selectedProducts =( product)=>{
+//     return {
+//         type: ActionTypes.SELECTED_PRODUCTS,
+//         payload: product,
+//     };
+// };
 
 export const removeSelectedProducts =( product)=>{
     return {
